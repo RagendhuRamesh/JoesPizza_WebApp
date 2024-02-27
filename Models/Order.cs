c3b541069d52684aac97b95092d4b8cb710c222a
RagendhuRamesh <ragendhuram@gmail.com>
2024-02-28 00:54:36 +0530

    First Commit

---
+++ Models/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAppPizza.Models
+{
+    public class Order
+    {
+        public string? Pizza { get; set; }
+        public int Quantity { get; set; }
+        public double Amount { get; set; }
+    }
+}
